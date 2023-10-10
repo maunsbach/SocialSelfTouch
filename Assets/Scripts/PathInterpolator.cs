c422e54e103ef08625a55bc3593ffa03e8838126
Martin Maunsbach <mama@di.ku.dk>
2023-10-10 12:34:39 +0200

    Dynamic Frequency Capping
    Capped rendering frequency. Major improvement to smooth haptic sensation.

--- Assets/Scripts/PathInterpolator.cs
+++ Assets/Scripts/PathInterpolator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,24 +8,61 @@ public class PathInterpolator : MonoBehaviour
     public CoordinateSpaceTransformer ReceiverCoordinateSpaceTransformer;
     public CoordinateSpaceTransformer SenderCoordinateSpaceTransformer;
 
-    public float MaxPathLength = 0.2f;
     public float DefaultInterpolationSeparation = 0.00035f;
-    private float _dynamicInterpolationSeparation;
+    private float _interpolationSeparation;
 
     public float MinimumFrequency = 60f;
-    private float _minimumInterpolationSeparation;
+    private float _maximumDistance;
+
     public float MaximumFrequency = 200f;
-    private float _maximumInterpolationSeparation;
+    private float _minimumDistance;
+
+    private float _uHSampleRate = 40000;
 
     private List<Vector3> InterpolatedPath = new List<Vector3>();
 
+    public bool DynamicCapping = true;
+
+    private void Awake()
+    {
+        _minimumDistance = DefaultInterpolationSeparation * _uHSampleRate / MaximumFrequency;
+        _maximumDistance = DefaultInterpolationSeparation * _uHSampleRate / MinimumFrequency;
+    }
+
     public void InterpolatePath(List <Vector3> path, float distance)
     {
+        if (DynamicCapping == true)
+        {
+            _interpolationSeparation = FetchInterpolationSeparationFromDistance(distance);
+        }
+        else
+        {
+            _interpolationSeparation = DefaultInterpolationSeparation;
+        }
 
         InterpolatedPath.Clear();
-        InterpolatedPath = MathT.InterpolatePaths(path, DefaultInterpolationSeparation);
+        InterpolatedPath = MathT.InterpolatePaths(path, _interpolationSeparation);
 
         ReceiverCoordinateSpaceTransformer.TransformPath(new List<Vector3>(InterpolatedPath));
         SenderCoordinateSpaceTransformer.TransformPath(new List<Vector3>(InterpolatedPath));
+    }
+
+    private float FetchInterpolationSeparationFromDistance(float distance)
+    {
+        _minimumDistance = DefaultInterpolationSeparation * _uHSampleRate / MaximumFrequency;
+        _maximumDistance = DefaultInterpolationSeparation * _uHSampleRate / MinimumFrequency;
+
+        if (distance < _minimumDistance)
+        {
+            return MaximumFrequency * distance / _uHSampleRate;
+        }
+        else if (distance > _maximumDistance)
+        {
+            return MinimumFrequency * distance / _uHSampleRate;
+        }
+        else
+        {
+            return DefaultInterpolationSeparation;
+        }
     }
 }
--- Assets/Scripts/PathTSP/PathRenderingController.cs
+++ Assets/Scripts/PathTSP/PathRenderingController.cs
@@ -7,42 +7,19 @@ using UnityEngine;
 public class PathRenderingController : MonoBehaviour
 {
     public PathInterpolator PathInterpolator;
-    public bool IsSender;
 
     public float MinSmoothingSeparation = 0.007f;
 
-    private Vector3 UltraLeapAlignment = new Vector3(0f, 0.1210f, 0f);
-
-    private Vector3 _transformPosition;
-    public Transform PointOffset;
-
     private List<Vector3> Points = new List<Vector3>();
-    //public List<Vector3> PointsPreTSP;
-    public List<Vector3> DebugPointsTSP;
-
-    //public List<Vector3> Cluster1Points = new List<Vector3>();
-    //public List<Vector3> Cluster2Points = new List<Vector3>();
-
-    public List<List<Vector3>> Paths = new List<List<Vector3>>();
-    public List<float> PathLengths = new List<float>();
-
-    //private List<Vector3> InterpolatedPath = new List<Vector3>();
-
 
     private void StartTSP(List<Vector3> contacts)
     {
-        //Points = contacts;
-        Paths.Clear();
-
         MathT.SmoothPoints(contacts, MinSmoothingSeparation);
 
         Points = new List<Vector3>(contacts);
-
         Points.Add(Points[0]);
-        float distance = MathT.TwoOpt(ref Points);
-        //TwoOptRecursive(Points);
 
-        //DebugPointsTSP = new List<Vector3>(Points);
+        float distance = MathT.TwoOpt(ref Points);
 
         PathInterpolator.InterpolatePath(Points, distance);
     }
@@ -51,7 +28,6 @@ public class PathRenderingController : MonoBehaviour
     {
         if (contactPoints.Count == 0)
         {
-            Paths.Clear();
             Points.Clear();
             PathInterpolator.InterpolatePath(new List<Vector3>(), 0f);
             return;
@@ -71,34 +47,4 @@ public class PathRenderingController : MonoBehaviour
 
         return contacts;
     }
-
-    /*private void TwoOptRecursive(List<Vector3> path)
-    {
-        float distance = MathT.TwoOpt(ref path);
-        Debug.Log(distance);
-
-        if (distance < MaxPathLength || path.Count <= 3)
-        {
-            path.Add(path[0]);
-            Paths.Add(path);
-        }
-        else 
-        {
-            KMeansResults result = KMeans.Cluster(path, 2, 50, 0);
-
-            List<Vector3> cluster1 = new List<Vector3>();
-            for (int i = 0; i < result.clusters[0].Length; i++)
-            {
-                cluster1.Add(path[result.clusters[0][i]]);
-            }
-            TwoOptRecursive(cluster1);
-
-            List<Vector3> cluster2 = new List<Vector3>();
-            for (int i = 0; i < result.clusters[1].Length; i++)
-            {
-                cluster2.Add(path[result.clusters[1][i]]);
-            }
-            TwoOptRecursive(cluster2);
-        }
-    }*/
 }
